161259841b6e08cf7ffb11b2cc148062b6591cad
Colin Maudry <colin@maudry.com>
2024-05-27 17:03:22 +0200

    use decp.info as label

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,11 +9,11 @@ do
 	git checkout $branch
 	sed -i -E "s/(SOURCE_SUM=)[a-f0-9]+/\1${hash:0:64}/" conf/app.src
 	git commit -am "Updated hash"
-	git push origin $branch
+	git push origin $branchsh
 
 done
 
-ssh yuno_root 'yunohost app install -f https://github.com/ColinMaudry/decp_datasette_ynh/tree/main --label decp-datasette-prod --args "domain=decp.info&path=/&is_public=true"'
-ssh yuno_root 'yunohost app install -f https://github.com/ColinMaudry/decp_datasette_ynh/tree/develop --label decp-datasette-test --args "domain=test.decp.info&path=/&is_public=true"'
+ssh yuno_root 'yunohost app install -f https://github.com/ColinMaudry/decp_datasette_ynh/tree/main --label decp.info-prod --args "domain=decp.info&path=/&is_public=true"'
+ssh yuno_root 'yunohost app install -f https://github.com/ColinMaudry/decp_datasette_ynh/tree/develop --label decp.info-test --args "domain=test.decp.info&path=/&is_public=true"'
 
 
